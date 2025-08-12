40e418822fbb325396cb67c37ad90fb2537c037e
Paloma Coqueiro <coqueiro214@gmail.com>
2025-08-12 12:53:14 +0000

    exxlivro1.c

---
+++ src/exclivro1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+    int a; 
+    for (a = 0 ; a <= 100 ; a++){
+        printf("%d\n" ,a);
+
+
+    }
+    return 0;
+}
--- src/if 4.c
+++ src/if 4.c
@@ -1,6 +1,6 @@
 #include <stdio. h>
 
-int main() {
+int main(){
     int numero;
 
     printf("Digite um número: ");
